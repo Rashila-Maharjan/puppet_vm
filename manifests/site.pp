3e3128304ccad356569b35d925f3874412e49c2e
rashila <rashila@linuxmaster.com>
2016-05-27 02:54:43 -0400

    Added file resource to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,14 @@
 node 'rashila.linuxmaster.com' {
-   include sys_info
+#   include sys_info
+}
+
+node 'vm01.linuxmaster.com' {
+ file {'/tmp/testserver':
+      content => "This is a test server.\n",
+ }
+}
+
+node /^vm[0-9]+\.linuxmaster\.com$/ {
+# include vim_upgrade
+# include sshd
 }
